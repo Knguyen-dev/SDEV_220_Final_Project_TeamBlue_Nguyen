43a28261199fb4b5ba11c0232a5541a1c9c8e388
Kevin Nguyen <thenoc27@gmail.com>
2023-04-13 08:56:34 -0400

    Finished a draft of shopping cart class and updated User
    We finished a general shopping cart class, and for user we replaced add/subtract balance functions with set functions for userPoints and userBalance. Also removed unnecessary error handling from the User class, and made it so User doesn't have a ShoppingCart instance as an attribute

--- User.py
+++ User.py
@@ -1,78 +1,75 @@
-# Class representing what individual users or consumer accounts may look like 
-
 # NOTE: Just a draft, not set in stone; since we're using tkinter and databases our code is likely to change a lot if we tihnk about how things will work more
+
+
+# Class representing what individual users or consumer accounts may look like 
 class User:
-		def __init__(self, username, firstName, lastName, shippingAddress, emailAddress):
-				
-				# The "_" to just indicate that it's a private variable (can't be accessed outside of class, must use methods to do so), 
-				# this is just convention as python doesn't actually have private variables, so I'm just doing to indicate
-
-				# Initialize User attributes that the user will input in a form or something similar
-				self._username = username 
-				self._firstName = firstName 
-				self._lastName = lastName
-				self.shippingAddress = shippingAddress  
-				self._emailAddress = emailAddress 
-				
-				# When we create accounts, the points and amount of money is going to be 0, accounts don't start off with money
-				# As well as this recentPUrchases is empty
-				self._userPoints = 0 
-				self._userBalance = 0 
-				self._recentPurchases = [] # list of integers that represent id for purchase table
-
-				# Represents the one shopping cart instance instance that they each User is linked to 
+	def __init__(self, username, firstName, lastName, shippingAddress, emailAddress):				
+		# Initialize User attributes that the user will input in a form or something similar; "_" just indicates private attribute
+		self._username = username 
+		self._firstName = firstName 
+		self._lastName = lastName
+		self._shippingAddress = shippingAddress  
+		self._emailAddress = emailAddress 
 		
-		# Setter functions to change attributes of User instances; obviously include functionality to save the new information to a database when finished.
-		def setUsername(self, newUsername):
-			self._username = newUsername
-		def setFirstName(self, newFirstName): # later have functionality that only accepts alphabetic characters
-			self._firstName = newFirstName
-		def setLastName(self, newLastName):
-			self._lastName = newLastName
-		def setShippingAddress(self, newShippingAddress):
-			self._shippingAddress = newShippingAddress
-		def setEmailAddress(self, newEmailAddress): # it's easier to assume that they enter a valid email
-			self._emailAddress = newEmailAddress
+		# When we create accounts, the points and amount of money is going to be 0, accounts don't start off with money
+		# As well as this recentPUrchases is empty
+		self._userPoints = 0 
+		self._userBalance = 0 
+		self._recentPurchases = [] # list of integers that represent id for purchase table
 
-		# For when the user is getting or using points on their account
-		def addUserPoints(self, amount):
-			self._userPoints += amount
-		def subtractUserPoints(self, amount): # May need error handling to make sure userPoints doesn't go below zero
-			self._userPoints  -= amount
-		
-		# For when the user is spending or adding money to their account
-		def addUserBalance(self, amount):
-			self._userBalance += amount
-		def subtractUserBalance(self, amount):
-			self._userBalance -= amount
-		
-		# Adds a purchase id to the front of the recentPurchases array 
-		def addRecentPurchases(self, newPurchase):
-			self._recentPurchases.insert(0, newPurchase) 
-		# Here we simulate removing a purchase from the back of the array, maybe when there's more than a certain amount it removes the oldest one
-		def removeRecentPurchase(self):
-			self.recentPurchases.pop()
+		# NOTE: Add shopping cart attribute which is another class for one to one relationship
+		# Represents the one shopping cart instance instance that they each User is linked to 
 
+	# Setter functions to change attributes of User instances; obviously include functionality to save the new information to a database when finished.
+	def setUsername(self, newUsername):
+		self._username = newUsername
+	def setFirstName(self, newFirstName): # later have functionality that only accepts alphabetic characters
+		self._firstName = newFirstName
+	def setLastName(self, newLastName):
+		self._lastName = newLastName
+	def setShippingAddress(self, newShippingAddress):
+		self._shippingAddress = newShippingAddress
+	def setEmailAddress(self, newEmailAddress): # it's easier to assume that they enter a valid email
+		self._emailAddress = newEmailAddress
 
-		
-		# Getter functions to get user instance attributes
-		def getUsername(self):
-			return self._username
-		def getFirstName(self):
-			return self._firstName
-		def getLastName(self):
-			return self._lastName
-		def getShippingAddress(self):
-			return self._shippingAddress
-		def getEmailAddress(self):
-			return self._emailAddress
-		def getPoints(self):
-			return self._userPoints
-		def getBalance(self):
-			return self._userBalance
+	# NOTE: userPoints or userBalance should never be below zero
+	def setUserPoints(self, amount):
+		self._userPoints = amount
+
+	def setUserBalance(self, amount):
+		self._userBalance = amount
+	
+	# Adds a purchase id to the front of the recentPurchases array 
+	def addRecentPurchases(self, newPurchase):
+		self._recentPurchases.insert(0, newPurchase) 
+	# Here we simulate removing a purchase from the back of the array, maybe when there's more than a certain amount it removes the oldest one
+	def removeRecentPurchases(self):
+		self.recentPurchases.pop()
+
+	# Getter functions to get user instance attributes
+	def getUsername(self):
+		return self._username
+	def getFirstName(self):
+		return self._firstName
+	def getLastName(self):
+		return self._lastName
+	def getShippingAddress(self):
+		return self._shippingAddress
+	def getEmailAddress(self):
+		return self._emailAddress
+	def getPoints(self):
+		return self._userPoints
+	def getBalance(self):
+		return self._userBalance
+	def getRecentPurchases(self):
+		return self._recentPurchases
+
+	# Function prints "User" instances by showing this string 
+	def __repr__(self):
+		return f"Username: {self._username}\nFirst name: {self._firstName}\nLast name: {self._lastName}\nShipping Address: {self._shippingAddress}\nEmail Address: {self._emailAddress}\nBalance: {self._userBalance}\nPoints: {self._userPoints}"
 		
 		
 def main():
-	stuff = "Just the starting message in main"
-	print(stuff)
+	user1 = User("David42", "David", "Bills", "123 English CT", "DB82@gmail.com")
+	
 main()
