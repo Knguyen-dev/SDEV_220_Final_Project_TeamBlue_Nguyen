dd7384699b492907fb686fe424eb5c4897ede320
Anthony Aldrich <123519936+AAldrich16@users.noreply.github.com>
2023-04-19 13:46:11 -0400

    Add files via upload

---
+++ pages/cartPage.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import Scrollbar, ttk
+import ttkbootstrap as tkb
+from ttkbootstrap.constants import *
+from ttkbootstrap.style import Bootstyle
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+import io
+import sqlite3
+
+class cartPage(tk.Frame):
+    def __init__(self, master, app):
+        super().__init__(master)
+        self.app = app
+        self.content = tkb.Frame(self.app)
+        self.content.place(relx=.5, rely=0.55, anchor="c")
+
+        self.cartLabel = tkb.Label(self.content, text="Welcome to your cart")
+        self.cartLabel.pack()
+
